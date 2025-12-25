baf702f44dd94a637208a3de2ea1035585eb0a2d
techcoms <120088252+techcoms@users.noreply.github.com>
2025-12-26 00:50:04 +0530

    Update Option.js

--- options/models/Option.js
+++ options/models/Option.js
@@ -1,6 +1,12 @@
-const mongoose = require('mongoose');
-const OptionSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  extraPrice: { type: Number, default: 0 }
-});
-module.exports = mongoose.model('Option', OptionSchema);
+const mongoose = require('./Model');
+
+const optionSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+    category: { type: String },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Option', optionSchema);
