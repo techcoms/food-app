9dc6f5f736075aa9743cc2acebea108d2eaaf4b8
techcoms <120088252+techcoms@users.noreply.github.com>
2025-12-26 11:54:01 +0530

    Update FoodItem.js

--- src/components/FoodItem.js
+++ src/components/FoodItem.js
@@ -1,12 +1,22 @@
 import React from 'react';
 
-export default function FoodItem({ food, addToCart }) {
+export default function FoodItem({ food = {}, addToCart = () => {} }) {
+  const price = new Intl.NumberFormat('en-IN', {
+    style: 'currency',
+    currency: 'INR',
+  }).format(food.price || 0);
+
   return (
     <div className="card">
       <h4>{food.name}</h4>
       <p>{food.description}</p>
-      <div>Price: ₹{food.price}</div>
-      <button onClick={() => addToCart(food)}>Add</button>
+      <div>Price: {price}</div>
+      <button
+        disabled={!food.price}
+        onClick={() => addToCart(food)}
+      >
+        Add
+      </button>
     </div>
   );
 }
